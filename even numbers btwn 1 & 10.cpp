076b4124ada7d6622aaa164a38d05baa0589f02c
KIPNGETICH-JARED <113914640+KIPNGETICH-JARED@users.noreply.github.com>
2022-09-29 18:11:36 +0300

    Add files via upload

---
+++ even numbers btwn 1 & 10.cpp
@@ -0,0 +1,11 @@
+//program to execute even numbers between 1 and 10
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=2;i<=10;i=i+2)
+    {
+        cout<<i<<"";
+    }
+    return 0;
+}
