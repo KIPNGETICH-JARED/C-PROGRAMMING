a793f1c64c503b2e450004fd79a1a9cfe86445d1
KIPNGETICH-JARED <113914640+KIPNGETICH-JARED@users.noreply.github.com>
2022-10-04 12:59:38 +0300

    Add files via upload
    while operation

---
+++ while for numbers between 1 & 10.cpp
@@ -0,0 +1,12 @@
+//program to print numbers betwen 1 and 10
+#include<iostream>
+using namespace std;
+int main(){
+    int i=10;
+    while(i>0)
+    {
+        cout<<i<<endl;
+        --i;
+    }
+    return 0;
+}
