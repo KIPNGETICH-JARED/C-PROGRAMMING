3dd12eb36ff2091bb8495240bc73a8f6e616789e
KIPNGETICH-JARED <113914640+KIPNGETICH-JARED@users.noreply.github.com>
2022-10-05 13:30:33 +0300

    Add files via upload
    program for do -while

---
+++ do while for odd numbers .cpp
@@ -0,0 +1,12 @@
+// program to print odd numbers from 1 to 10
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i=i+2;
+    }
+    while(i<=10);
+    return 0;
+}
